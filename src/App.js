81bf7752f95cc5c3fe73895f249fd2523923f674
Jayden Thrasher <JaydenThrasher@gmail.com>
2020-03-13 17:02:28 -0400

    Use the Button component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ButtonComponent from "./ButtonComponent";
+import { connect } from "react-redux";
 
-function App() {
+function mapStateToProps(state) {
+  return { state };
+}
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={props.state.status ? styles.day : styles.night}>
+      <ButtonComponent />
     </div>
   );
 }
 
-export default App;
+const styles = {
+  day: {
+    backgroundColor: "#E0FEF0",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  night: {
+    backgroundColor: "#1f263b",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+};
+
+export default connect(mapStateToProps)(App);
