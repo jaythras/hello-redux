f09d6121fd188478f21bb4a50fe74c43a36bd6d8
Jayden Thrasher <JaydenThrasher@gmail.com>
2020-03-13 17:00:46 -0400

    Connect store to the App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { toggle } from "./actions";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer);
+
+// Only for demonstration purposes. Do NOT leave this in an actual project.
+window.store = store;
+window.toggle = toggle;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
